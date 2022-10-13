a6e935c2ce171add4233329b301c23880c3e621d
DRenZ777 <reihannaufal3@gmail.com>
2022-10-13 15:54:29 +0700

    Update

--- app/Views/Sertifikat.php
+++ app/Views/Sertifikat.php
@@ -2,6 +2,175 @@
 
 <?= $this->section('content'); ?>
 
-<h1>sertifikat</h1>
+<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 ms-2 me-2 g-3 mb-3 mt-3">
+    <div class="col mb-3">
+        <div class="card">
+            <div class="card-header">
+                Sertifikat
+            </div>
+            <div class="card-body">
+                <table>
+                    <tr>
+                        <th>
+                            <h5 class="card-title font-weight-bold text-primary">Sertifikat AC010</h5>
+                        </th>
+                    </tr>
+
+                    <tr>
+                        <br>
+                        <td>
+                            <p class="card-text">Nama </p>
+
+                        </td>
+                        <td>
+                            <p class="card-text">: Cristiano Ronaldo</p>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <p class="card-text">NPM </p>
+
+                        </td>
+                        <td>
+                            <p class="card-text">: 0619151510</p>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <p class="card-text">Reguler </p>
+
+                        </td>
+                        <td>
+                            <p class="card-text">: Reg A</p>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <p class="card-text">Tanggal Ujian </p>
+
+                        </td>
+                        <td>
+                            <p class="card-text">: 27 Oktober 2021</p>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <p class="card-text">Grade </p>
+                        </td>
+                        <td>
+                            <p class="card-text">: 85</p>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <p class="card-text">Result </p>
+                        </td>
+                        <td>
+                            <p class="card-text">: Passed</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p class="card-text">Harga </p>
+                        </td>
+                        <td>
+                            <p class="card-text">: Rp. 200.000</p>
+                        </td>
+                    </tr>
+                </table>
+                <a href="#" class="btn btn-primary">Ambil Sertifikat</a>
+            </div>
+        </div>
+    </div>
+    <div class="col mb-3">
+        <div class="card">
+            <div class="card-header">
+                Sertifikat
+            </div>
+            <div class="card-body">
+                <table>
+                    <tr>
+                        <th>
+                            <h5 class="card-title font-weight-bold text-primary">Sertifikat AB10</h5>
+                        </th>
+                    </tr>
+
+                    <tr>
+                        <br>
+                        <td>
+                            <p class="card-text">Nama </p>
+
+                        </td>
+                        <td>
+                            <p class="card-text">: Cristiano Ronaldo</p>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <p class="card-text">NPM </p>
+
+                        </td>
+                        <td>
+                            <p class="card-text">: 0619151510</p>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <p class="card-text">Reguler </p>
+
+                        </td>
+                        <td>
+                            <p class="card-text">: Reg A</p>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <p class="card-text">Tanggal Ujian </p>
+
+                        </td>
+                        <td>
+                            <p class="card-text">: 27 Oktober 2021</p>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <p class="card-text">Grade </p>
+                        </td>
+                        <td>
+                            <p class="card-text">: 85</p>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <p class="card-text">Result </p>
+                        </td>
+                        <td>
+                            <p class="card-text">: Passed</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p class="card-text">Harga </p>
+                        </td>
+                        <td>
+                            <p class="card-text">: Rp. 200.000</p>
+                        </td>
+                    </tr>
+                </table>
+                <a href="#" class="btn btn-primary">Ambil Sertifikat</a>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?= $this->endsection(); ?>
